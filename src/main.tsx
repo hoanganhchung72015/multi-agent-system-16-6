dde713897b349db296889c49105b52015cd9d8a4
hoanganhchung72015 <hoanganhchung72015@gmail.com>
2025-12-28 20:46:15 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
 
 // Lệnh này tìm thẻ 'root' trong index.html và render App vào đó
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+
 )
